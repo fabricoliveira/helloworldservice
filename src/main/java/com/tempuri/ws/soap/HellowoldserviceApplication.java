4f5891df11bec721b5cbf7bf841e1a82642e3b09
fabriciooliveira <oliveira.fabricio.santos@gmail.com>
2019-04-24 15:32:51 -0300

    first commit

---
+++ src/main/java/com/tempuri/ws/soap/HellowoldserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tempuri.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowoldserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowoldserviceApplication.class, args);
+	}
+
+}
